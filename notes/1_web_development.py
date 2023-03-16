fc92a996170accb6b0739a261d9355321d290ef7
sakuexe <saku.karttunen@gmail.com>
2023-03-16 16:33:57 +0200

    added the first notes and todo app example

---
+++ notes/1_web_development.py
@@ -0,0 +1,27 @@
+# Backend Web Frameworks using Python
+python_frameworks = [
+    "Django",
+    "Flask",
+    "Bottle",
+    "Falcon",
+    "Tornado",
+    "Sanic"
+]
+
+# Frontend Web Libraries / Frameworks using JavaScript
+javascript_frameworks = {
+    "React": "JSX / TSX",
+    "Vue": "Javascript",
+    "Angular": "Typescript"
+}
+
+# Other Web Programming Languages
+other_langs = [
+    "PHP",
+    # Drupal, Wordpress, Joomla, Symfony, CakePHP, Laravel
+    "Ruby on Rails",
+    "Java",
+    # Spring, Play
+    "C#"
+    # ASP.NET, DotNetNuke (DNN) - CMS, Umbraco - CMS
+]
