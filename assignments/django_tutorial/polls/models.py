7166c956b8845455fb4e8e861376f2740f89b8eb
sakuexe <saku.karttunen@gmail.com>
2023-03-28 10:05:50 +0300

    finished django tutorial part 2

--- assignments/django_tutorial/polls/models.py
+++ assignments/django_tutorial/polls/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import datetime
+
+from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# creating a new table for questions
+class Question(models.Model):
+    """ the columns for the table,
+        Django automatically creates IDs
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    # Python's ToString() method
+    def __str__(self):
+        return self.question_text
+    # class method for checking if the question was published within the last
+    # 24 hours
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+# create another table for choices
+class Choice(models.Model):
+    # refers to another table (that being the question)
+    # also on deletion will get removed because of the last argument
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
