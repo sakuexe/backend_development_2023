eb89237e5af71a62050146dfeaf848478ccd9653
sakuexe <saku.karttunen@gmail.com>
2023-03-31 09:43:06 +0300

    finished django tutorial part 5

--- assignments/django_tutorial/polls/models.py
+++ assignments/django_tutorial/polls/models.py
@@ -21,7 +21,8 @@ class Question(models.Model):
     # 24 hours
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 # create another table for choices
